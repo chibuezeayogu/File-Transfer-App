7067e3dcc8944f322768d0c38364d15d040eaaa4
Chibueze Nelson Ayogu <chibuezeayogu@hotmail.com>
2019-03-02 23:43:34 +0100

    add and configure Bootstrap (#5)
    -[Finishes #164197198]

--- spec/sample_test_spec.rb
+++ spec/sample_test_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
- RSpec.describe 'Sample Test' do
-  it "testing" do
+RSpec.describe 'Sample Test' do
+  it 'testing' do
     expect(true).to eq true
   end
 end
